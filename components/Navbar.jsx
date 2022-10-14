60fff5fe6403415e15724538c27d5dd0cc0afecb
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-10-13 17:29:44 -0700

    navbar work

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,10 @@
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div classname='fixed w-full h-20 shadow-xl'>
+            <div classname='flex justify-between items-cente w-full h-full px-2 2xl:px-16'>
+
+            </div>
+        </div>
     )
 }
 
